a188265d0e19577f7b4872aef0fd869ac95ecbb6
ahmadzarkasih01 <ahmadzarkasyiilinggau2019@gmail.com>
2025-01-30 13:40:01 +0700

    add-page-archives

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import ListNotes from "./pages/HomePage";
+import ArchivesPage from "./pages/ArchivesPage";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
       <main>
         <Routes className="app-container">
           <Route path="/" element={<ListNotes />} />
+          <Route path="/archives" element={<ArchivesPage />} />
         </Routes>
       </main>
     </>
